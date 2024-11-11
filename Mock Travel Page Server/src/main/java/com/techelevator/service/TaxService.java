3437f03206dc6ab43b5b15a93208c70c00f8a1bd
hakther15 <92819734+hakther15@users.noreply.github.com>
2024-11-10 20:18:09 -0500

    removed parts of a project
    repeated project already in github

--- Mock Travel Page Server/src/main/java/com/techelevator/service/TaxService.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator.service;
-
-import com.techelevator.model.TaxServiceDto;
-
-//Interface used for RestTaxService
-public interface TaxService {
-
-    TaxServiceDto getTaxRate(String stateCode);
-}
